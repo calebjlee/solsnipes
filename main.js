3f26326e99948f6a6cc1fba0a0088042b71f52f5
Anthony Varkey <anthonyvarkey@gmail.com>
2024-05-06 11:55:39 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+const TelegramBot = require('node-telegram-bot-api');
+
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_SECRET;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    console.log(msg?.text);
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
