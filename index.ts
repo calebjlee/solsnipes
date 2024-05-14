d52f03f509dc897fa876425a89226047c22b67d6
calebjl <calebjl@umich.edu>
2024-05-14 13:48:17 -0700

    updated message

--- index.ts
+++ index.ts
@@ -25,7 +25,9 @@ async function main() {
         targetGainPercentage: 50, //Adjust this to how much profit we want to take i. e. 50 = 50% profit
         initialInputToken: SwapToken.SOL,
         initialInputAmount: 0.005 * LAMPORTS_PER_SOL, // Adjust this to how much sol we want to initially invest. 0.005 = 0.005 SOL
+        // For Tony: Pass in the mint address of the token you get from telegram.
         initialAltMint: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
+        
     });
 
     await bot.init();
